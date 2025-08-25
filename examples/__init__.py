3bd5ef4508d7e638d42303f5fe6a1e762f893f46
Michael Mace <michael.mace@conexon.us>
2025-08-25 14:40:26 -0500

    add init to examples

---
+++ examples/__init__.py
@@ -0,0 +1,9 @@
+# this file makes the examples directory a python package
+# all example modules will be imported here to register their routes
+
+# import all example modules to register their page routes
+from . import _01_hello_world
+from . import _02_counter
+from . import _03_loading
+from . import _04_streaming
+from . import _05_multi_page
